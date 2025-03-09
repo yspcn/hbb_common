8472e805974dc3e79795d7e8fb5d1fd9289578da
yspcn <55094985+yspcn@users.noreply.github.com>
2025-03-09 11:48:55 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -81,8 +81,8 @@ lazy_static::lazy_static! {
     pub static ref APP_HOME_DIR: RwLock<String> = Default::default();
 }
 
-pub const LINK_DOCS_HOME: &str = "https://rustdesk.com/docs/en/";
-pub const LINK_DOCS_X11_REQUIRED: &str = "https://rustdesk.com/docs/en/manual/linux/#x11-required";
+pub const LINK_DOCS_HOME: &str = "https://rustdesk.com/docs/zh-cn/";
+pub const LINK_DOCS_X11_REQUIRED: &str = "https://rustdesk.com/docs/zh-cn/client/mac/";
 pub const LINK_HEADLESS_LINUX_SUPPORT: &str =
     "https://github.com/rustdesk/rustdesk/wiki/Headless-Linux-Support";
 lazy_static::lazy_static! {
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.aijinju.com"];
+pub const RS_PUB_KEY: &str = "BbrD4OL6p7Ctz3dbtMtW+2ejV8WoGnWckjNzDI+v730=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
